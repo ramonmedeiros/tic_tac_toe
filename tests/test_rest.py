13a6f00b6a50d6834155130090af08df9d6531e4
Ramon Medeiros <ramon.rnm@gmail.com>
2019-03-27 10:50:37 +0100

    use validators and do a move from rest api

--- tests/test_rest.py
+++ tests/test_rest.py
@@ -4,15 +4,33 @@ from tic_tac_toe import app, log
 logger = log.getLogger()
 log.set_verbosity(log.DEBUG)
 
+
 class TestRest(TestCase):
-    
     @classmethod
     def setUpClass(self):
         app.app.config['TESTING'] = True
         self.app = app.app.test_client()
 
-
     def test_hello(self):
         rv = self.app.get('/')
-        assert(rv.data.decode() == 'Tic Tac Toe by Ramon Medeiros')
+        assert (rv.data.decode() == app.TITLE)
 
+    def test_start_new_game(self):
+        rv = self.app.post('/game')
+        uid = rv.data.decode()
+        assert (rv.status_code == 201)
+
+        rv = self.app.get('/game/' + uid)
+        assert (rv.status_code == 200)
+
+    def test_reach_non_existing_game(self):
+        assert (self.app.get('/game/not-exists').status_code == 404)
+
+    def test_do_move(self):
+        new_game = self.app.post('/game')
+        move = self.app.post('/game/' + new_game.data.decode(), data={"column": 0, "line": 0, "player": 0})
+        assert(move.status_code == 200)
+
+        board = self.app.get('/game/' + new_game.data.decode())
+        logger.debug(board.data.decode())
+        
--- tic_tac_toe/app.py
+++ tic_tac_toe/app.py
@@ -4,6 +4,7 @@ from flask import Flask, request, make_response
 from tic_tac_toe.game import Game
 from tic_tac_toe import log
 from uuid import uuid4
+from wtforms import Form, FieldList, IntegerField, StringField, FormField, validators
 
 app = Flask("tic-tac-toe")
 games = {}
@@ -50,10 +51,35 @@ def game():
 @app.route("/game/<uuid>", methods=[GET, POST, DELETE])
 def deal_with_game(uuid: str):
     global games
+
+    # check if uuid is valid
+    this_game = games.get(uuid)
+
+    if this_game is None:
+        return make_response("not found", 404)
+
+    params = DoMoveParameters(request.form)
+    if request.method == POST: 
+        if params.validate():
+            logger.debug(params.errors)
+            return params.errors, 400
+
+        if this_game.do_move(params.column.data,
+                                 params.line.data,
+                                 params.player.data) is True:
+            return "moved", 200
+        return "failed", 403
+
     if request.method == GET:
         return pretty_print(games.get(uuid).get_board())
 
     elif request.method == DELETE:
         return games.delete(request.form["uuid"])
 
+    return make_response(f"{request.method} not implemented", 405)
 
+
+class DoMoveParameters(Form):
+    column = IntegerField(COLUMN, [validators.DataRequired()])
+    line =  IntegerField(LINE, [validators.DataRequired()])
+    player = IntegerField(PLAYER, [validators.DataRequired()])
