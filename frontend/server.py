efd7aa6fdf660b7aab123a5ad35c532c57bae3ea
Ramon Medeiros <ramon.rnm@gmail.com>
2019-04-01 11:50:35 +0200

    redirect to index case uuid not found

--- frontend/server.py
+++ frontend/server.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, make_response, jsonify
+import requests
+
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask("frotend", template_folder="frontend/templates", static_folder="frontend/static")
 
@@ -10,6 +12,9 @@ def index():
 
 @app.route("/game/<uuid>", methods=[GET])
 def play_game(uuid):
+    if requests.get(f"{get_backend_uri()}/game/{uuid}").status_code != 200:
+        return redirect(url_for("index"))
+    
     return render_template("game.html", uuid=uuid)
 
 def get_backend_uri():
